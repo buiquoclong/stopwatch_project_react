7d3b820f1313f7a65b590e7b8818aa8f6c113173
buiquoclong <buiquoclongkt123@gmail.com>
2026-03-17 15:21:02 +0700

    First commit in StopWatch Project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import StopWatch from "./stopwatch/StopWatch";
+
+function App() {
+  return (
+    <div>
+      <StopWatch></StopWatch>
+    </div>
+  );
+}
+
+export default App;
